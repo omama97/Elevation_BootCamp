e54da44e902a19301cc57d6be0029581772afa0d
omama <oy8395@gmail.com>
2022-07-14 13:13:43 -0700

    first and seconed week

--- Bootcamp/exercices/week-1/day5/DomIntro/main.js
+++
@@ -1,8 +0,0 @@
-console.log(document);
-console.dir(document);
-let playing_field = document.getElementById("playing-field");
-
-playing_field.style.backgroundColor = "blue";
-
-const down = document.getElementById("down");
-console.log(down);
